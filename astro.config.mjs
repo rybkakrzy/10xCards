801b03afa595932b824952ca2986643c6469990a
krdev <krzysztof.rybka@krdev.pl>
2026-01-19 14:34:57 +0100

    fix: popraweczka cz 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
 import path from "path";
@@ -11,10 +11,15 @@ import path from "path";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), sitemap()],
+  integrations: [
+    react(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   server: { port: 3000 },
   vite: {
-    plugins: [tailwindcss()],
     resolve: {
       alias: {
         "@": path.resolve("./src"),
